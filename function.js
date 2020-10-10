a6f0c1d7beb69d4d5f0498f94bb273ffa1c95e35
7 <7te@protonmail.ch>
2020-10-10 11:44:49 -0400

    ugh

--- function.js
+++ function.js
@@ -4,7 +4,8 @@ function flipSwitch() {
         document.querySelector('html').style.backgroundColor = 'rgb(250, 250, 250)';
         console.log('clrwasfafafa');
     }
-    if (document.querySelector('html').style.backgroundColor = 'rgb(250, 250, 250)') {             document.querySelector('html').style.backgroundColor = 'rgb(35, 35, 35)';
+    if (document.querySelector('html').style.backgroundColor = 'rgb(250, 250, 250)') {
+        document.querySelector('html').style.backgroundColor = 'rgb(35, 35, 35)';
         console.log('darkkk');
     }
 }
