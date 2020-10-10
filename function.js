3df9c6839baafa9bf1babc5b0663fff3678554cf
7 <7te@protonmail.ch>
2020-10-10 10:38:24 -0400

    testing

---
+++ function.js
@@ -0,0 +1,7 @@
+function flipSwitch() {
+    console.log('button triggered');
+    
+    if (document.querySelector('body').style.backgroundColor = '#fafafa')
+    {document.querySelector('body').style.backgroundColor = '#232323'}
+    else { document.querySelector('body').style.backgroundColor = '#fafafa'}
+}
